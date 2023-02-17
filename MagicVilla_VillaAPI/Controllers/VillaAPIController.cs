66c730627aed6c72892296cd4f9af384e7138042
tejas-trivedi <500077185@stu.upes.ac.in>
2023-02-17 12:01:43 +0530

    basic get request

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MagicVilla_VillaAPI.Model;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Data;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return VillaStore.villaList;
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Models/Villa.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace MagicVilla_VillaAPI.Model
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
